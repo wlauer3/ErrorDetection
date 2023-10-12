4cc42a9a94622ffd317599a473e0eb1cf4a58766
Lauer, William Davis <wlauer3@gatech.edu>
2023-10-12 16:02:44 -0400

    Select feature added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './index.css';
 
 function App() {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (button) => {
+    if (activeButton === button) {
+      setActiveButton(null); // Clicked the same button again, reset active button
+    } else {
+      setActiveButton(button); // Clicked a different button, set it as the active button
+    }
+  };
+
+  const isButtonActive = (button) => button === activeButton;
+
   return (
     <div className='window'>
-      <div className = "options">
-        <button className="button"> Checksum </button>
-        <button className="button"> Parity Check </button>
-        <button className="button"> Cyclic Redundancy Check </button>
+      <div className="options">
+        <button className={`button ${isButtonActive('CheckSum') ? 'clicked' : ''}`} onClick={() => handleButtonClick('CheckSum')}>Checksum</button>
+        <button className={`button ${isButtonActive('ParityCheck') ? 'clicked' : ''}`} onClick={() => handleButtonClick('ParityCheck')}>Parity Check</button>
+        <button className={`button ${isButtonActive('CyclicRedundancyCheck') ? 'clicked' : ''}`} onClick={() => handleButtonClick('CyclicRedundancyCheck')}>Cyclic Redundancy Check</button>
       </div>
       <div className='viewSpace'>
         <div className='viewWindow'>
-
+          
         </div>
       </div>
     </div>
